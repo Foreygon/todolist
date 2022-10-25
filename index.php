c56994c7e3bf7d8f7ca2fcac346f4bcf14ac95ca
Foreygon <damien.letiche@gmail.com>
2022-10-25 14:02:05 +0200

    phase 3, lier bdd

--- index.php
+++ index.php
@@ -1,31 +1,71 @@
-
 <?php
 require_once  "./view/_head.php";
-require_once "./outils/_erreur-saisie-todo.php";
-?>
+// require_once "./outils/_erreur-saisie-todo.php";
 
+
+$dsn = 'mysql:host=localhost;dbname=todolist';
+$user = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO($dsn, $user, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+    // echo "Connexion réussi";
+} catch (PDOException $e) {
+    echo "connexion échouée : " . $e->getMessage();
+}
+
+
+$errors = ['todo' => ''];
+$todo = ['tache' => ''];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $todo = filter_var($_POST['todo'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    // $stmt = $pdo->prepare("INSERT INTO todo VALUES (DEFAULT, $todo)");
+    $stmt = $pdo->prepare("INSERT INTO todo VALUES (DEFAULT, :tache)");
+
+
+
+    if (!$todo) {
+        $errors['todo'] = '<p class="color_red"><strong>Votre saisie contient aucun caractere</strong></p>';
+    } elseif (strlen($todo) <= 5) {
+        $errors['todo'] = '<p class="color_red"><strong>Votre saisie est trop courte</strong></p>';
+    } elseif (!$errors['todo']) {
+        $stmt->bindValue(':tache', $todo);
+        $stmt->execute();
+    }
+}
+
+
+?>
 <title>TodoList</title>
 
 </head>
+
 <body>
     <?php
     require_once "./view/_header.php";
-    ?> 
+    ?>
     <main class="contain">
         <div class="contain-form">
             <h2 class="h2-form">Ma Todo</h2>
             <form class="form" action="/" method="POST">
-                <label for="text" hidden>tu doit faire?</label>
-                <input class="input-form" type="text" name="text" id="text" placeholder="Je dois faire?">
+                <label for="todo" hidden>tu doit faire?</label>
+
+                <input class="input-form" type="text" name="todo" id="todo" placeholder="Tu dois faire?">
                 <button class="btn" type="submit">Ajouter</button>
             </form>
             <div class="errors dpf-jc">
-            <?php 
-                echo $errors['text'];
-            ?>
+                <?php
+                echo $errors['todo'];
+                ?>
             </div>
         </div>
     </main>
-<?php
-require_once  "./view/_footer.php";
-?>
+    <?php
+    require_once  "./view/_footer.php";
+    ?>
--- outils/_erreur-saisie-todo.php
+++ outils/_erreur-saisie-todo.php
@@ -1,14 +1,31 @@
 <?php 
-            $errors = [
-                'text'=> ''
-            ];
+
+        $dsn = 'mysql:host=localhost;dbname=todolist';
+        $user = 'root';
+        $password = '';
+
+        try {
+            $pdo = new PDO($dsn, $user, $password, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+            echo "Connexion réussi";
+        } catch (PDOException $e) {
+            echo "connexion échouée : " . $e->getMessage();
+        }
+
+
+            $errors = ['todo'=> ''];
+            $todo = ['tache'=>''];
+
             if ($_SERVER['REQUEST_METHOD']==='POST'){
-                $todo = filter_var($_POST['text'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $todo = filter_var($_POST['todo'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $stmt = $pdo->prepare("INSERT INTO todo VALUES (DEFAULT, '$todo')");
 
                 if (!$todo){
-                    $errors['text'] = '<p class="color_red"><strong>Votre saisie contient aucun caractere</strong></p>';
+                    $errors['todo'] = '<p class="color_red"><strong>Votre saisie contient aucun caractere</strong></p>';
                 } elseif (strlen($todo) <= 5){
-                    $errors['text'] = '<p class="color_red"><strong>Votre saisie est trop courte</strong></p>';
+                    $errors['todo'] = '<p class="color_red"><strong>Votre saisie est trop courte</strong></p>';
                 }
             }
-            ?>
+            // header('Location: http://localhost:3000/');
